87ec7d210cca8e5f5af8af6088cb9f09758ec716
Hazlinda <52389503+Hazlinda@users.noreply.github.com>
2019-07-02 10:03:13 +0800

    Create Join_none.sv

---
+++ EXAMPLE : fork/Join_none.sv
@@ -0,0 +1,25 @@
+//EXAMPLE : fork/join none 
+
+program main ; 
+initial 
+begin 
+#10; 
+$display(" BEFORE fork time = %d ",$time ); 
+fork 
+begin 
+# (20); 
+$display("time = %d # 20 ",$time ); 
+end 
+begin 
+#(10); 
+$display("time = %d # 10 ",$time ); 
+end 
+begin 
+#(5); 
+$display("time = %d # 5 ",$time ); 
+end 
+join_none 
+$display(" time = %d Outside the main fork ",$time ); 
+#(40); 
+end 
+endprogram 
